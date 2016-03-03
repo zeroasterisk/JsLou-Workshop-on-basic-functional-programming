b80bac486228b028fd288775466f09d6b97ece6d
alan bount <alan@zeroasterisk.com>
2016-03-02 21:16:15 -0500

    jslou workshop, exercises 1-5, done in session

---
+++ 01-hello-world.js
@@ -0,0 +1,23 @@
+// https://github.com/timoxley/functional-javascript-workshop
+// hello world
+
+/*
+
+# Task
+
+Write a function that takes an input string and returns it uppercased.
+
+## Arguments
+
+  * input: a String of random words (lorem ipsum).
+
+*/
+
+function upperCaser(input) {
+  return String(input).toUpperCase();
+}
+
+module.exports = upperCaser
+
+
+
